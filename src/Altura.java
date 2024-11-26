c2f4a5f2cf160f1fc95b065ddb74378df86e2257
Abelsabater <abesabmun@alu.edu.gva.es>
2024-11-26 14:49:29 +0100

    Estructuras basicas

--- src/Altura.java
+++ src/Altura.java
@@ -3,6 +3,7 @@ import java.util.Scanner;
 
 public class Altura {
 
+
     public static int numeroalumnos(){
         Scanner sc=new Scanner(System.in);
         System.out.println("Cuantos alumnos hay?");
