c8a7f474704eb01cb3064ff6a9476a8559f9f064
Abelsabater <abesabmun@alu.edu.gva.es>
2024-11-28 14:20:18 +0100

    Estructuras basicas

--- src/TelefonoII.java
+++ src/TelefonoII.java
@@ -14,13 +14,20 @@ public class TelefonoII {
     public static void main(String[] args) {
         Scanner sc=new Scanner(System.in);
         HashMap<String, ArrayList<Integer>> Guiatelefonos=new HashMap<>();
-        ArrayList<Integer>telefonos=new ArrayList<>();
+        ArrayList<Integer> telefonos=new ArrayList<>();
         telefonos.add(711460219);
         telefonos.add(629350754);
         telefonos.add(624918634);
-
         Guiatelefonos.put("ABEL",telefonos);
+        telefonos=new ArrayList<Integer>();
+        telefonos.add(607306135);
+        telefonos.add(635815376);
+        telefonos.add(636532275);
         Guiatelefonos.put("PAU",telefonos);
+        telefonos=new ArrayList<Integer>();
+        telefonos.add(743508243);
+        telefonos.add(651097606);
+        telefonos.add(650858984);
         Guiatelefonos.put("MANEL",telefonos);
         System.out.println("Dime una persona");
         String pers= sc.nextLine().toUpperCase();
