a53017215ecd8148014a95ce6ea55c949ca699ce
Abelsabater <abesabmun@alu.edu.gva.es>
2024-11-28 13:32:08 +0100

    Estructuras basicas

---
+++ src/PaisCapital.java
@@ -0,0 +1,25 @@
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Scanner;
+
+public class PaisCapital {
+
+    public static String paiscapital(Map<String, String>Paises, String pais){
+            return Paises.get(pais);
+    }
+
+
+    public static void main(String[] args) {
+        Map<String,String>Paises=new HashMap<String,String>();
+        Scanner sc=new Scanner(System.in);
+        Paises.put("España","Madrid");
+        Paises.put("Italia","Roma");
+        Paises.put("Francia","Paris");
+        Paises.put("Argentina","Buenos aires");
+        Paises.put("Alemania","Berlin");
+        System.out.println("Dime un pais");
+        String pais=sc.nextLine();
+        System.out.println(paiscapital(Paises,pais));
+    }
+}
---
+++ src/RepartiendoRegalos.java
@@ -0,0 +1,40 @@
+import java.util.ArrayList;
+import java.util.Scanner;
+
+public class RepartiendoRegalos {
+
+    public static String cerca(ArrayList<Integer> calles){
+        int cont;
+        int cerca=1000000000;
+        int calless=calles.size();
+        String orden="";
+        int pos=0;
+        int callact=calles.get(0);
+        do{
+            for (Integer cal: calles) {
+                cont=callact-calles.get(cal);
+                if(Math.abs(cont)<=cerca){
+                    cerca=cont;
+                    pos=cal;
+                }
+            }
+            callact=calles.get(pos);
+            orden+=" "+calles.get(pos);
+            calless--;
+            calles.remove(calles.get(pos));
+        }while(calless>0);
+        return orden;
+    }
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        ArrayList<Integer> calles=new ArrayList<>();
+        System.out.println("Quantas calles");
+        int ncal= sc.nextInt();
+        System.out.println("Posa el numero dels carrers");
+        for (int i = 0; i < ncal; i++) {
+            int cal= sc.nextInt();
+            calles.add(cal);
+        }
+        System.out.println(cerca(calles));
+    }
+}
