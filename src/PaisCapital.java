51ce0be49d4c0eb593f1de6dac8ee1d8ed923511
Abelsabater <abesabmun@alu.edu.gva.es>
2024-11-28 13:38:06 +0100

    Estructuras basicas

--- src/PaisCapital.java
+++ src/PaisCapital.java
@@ -5,18 +5,18 @@ import java.util.Scanner;
 public class PaisCapital {
 
     public static String paiscapital(Map<String, String>Paises, String pais){
-            return Paises.getOrDefault(pais,"No sabem la capital del pais o no existeix");
+        return Paises.getOrDefault(pais,"No sabem la capital del pais o no existeix");
     }
     public static void main(String[] args) {
         Map<String,String>Paises=new HashMap<String,String>();
         Scanner sc=new Scanner(System.in);
-        Paises.put("España","Madrid");
-        Paises.put("Italia","Roma");
-        Paises.put("Francia","Paris");
-        Paises.put("Argentina","Buenos aires");
-        Paises.put("Alemania","Berlin");
+        Paises.put("ESPAÑA","Madrid");
+        Paises.put("ITALIA","Roma");
+        Paises.put("FRANCIA","Paris");
+        Paises.put("ARGENTINA","Buenos aires");
+        Paises.put("ALEMANIA","Berlin");
         System.out.println("Dime un pais");
-        String pais=sc.nextLine();
+        String pais=sc.nextLine().toUpperCase();
         System.out.println(paiscapital(Paises,pais));
     }
 }
