c1cabec82d156ae028332d78fdbdf886c8cc5ffb
Abelsabater <abesabmun@alu.edu.gva.es>
2024-11-29 11:35:53 +0100

    Estructuras basicas

--- src/Telefono.java
+++ src/Telefono.java
@@ -4,21 +4,17 @@ import java.util.Scanner;
 
 public class Telefono {
 
-    public static void telefono(Map<String,Integer>Guiatelefono,String pers){
+    public static void telefono(Map<String,String>Guiatelefono,String pers){
         int telefono=0;
-        if(Guiatelefono.containsKey(pers)){
-            System.out.println(Guiatelefono.get(pers));
-        }else{
-            System.out.println("No tenim a eixa persona guardada");
-        }
+        System.out.println(Guiatelefono.getOrDefault(pers, "No tenim a eixa persona guardada"));
     }
 
     public static void main(String[] args) {
         Scanner sc=new Scanner(System.in);
-        Map <String,Integer> Guiatelefonos=new HashMap<>();
-        Guiatelefonos.put("ABEL",711460219);
-        Guiatelefonos.put("PAU",665426637);
-        Guiatelefonos.put("MANEL",725639857);
+        Map <String,String> Guiatelefonos=new HashMap<>();
+        Guiatelefonos.put("ABEL","711460219");
+        Guiatelefonos.put("PAU","665426637");
+        Guiatelefonos.put("MANEL","725639857");
         System.out.println("Dime una persona");
         String pers= sc.nextLine().toUpperCase();
         telefono(Guiatelefonos,pers);
--- src/TelefonoII.java
+++ src/TelefonoII.java
@@ -3,7 +3,7 @@ import java.util.HashMap;
 import java.util.Scanner;
 
 public class TelefonoII {
-    public static void telefono(HashMap<String, ArrayList<Integer>> Guiatelefono, String pers){
+    public static void telefono(HashMap<String, ArrayList<String>> Guiatelefono, String pers){
         if(Guiatelefono.containsKey(pers)){
             System.out.println(Guiatelefono.get(pers));
         }else{
@@ -13,21 +13,21 @@ public class TelefonoII {
 
     public static void main(String[] args) {
         Scanner sc=new Scanner(System.in);
-        HashMap<String, ArrayList<Integer>> Guiatelefonos=new HashMap<>();
-        ArrayList<Integer> telefonos=new ArrayList<>();
-        telefonos.add(711460219);
-        telefonos.add(629350754);
-        telefonos.add(624918634);
+        HashMap<String, ArrayList<String>> Guiatelefonos=new HashMap<>();
+        ArrayList<String> telefonos=new ArrayList<>();
+        telefonos.add("711460219");
+        telefonos.add("629350754");
+        telefonos.add("624918634");
         Guiatelefonos.put("ABEL",telefonos);
-        telefonos=new ArrayList<Integer>();
-        telefonos.add(607306135);
-        telefonos.add(635815376);
-        telefonos.add(636532275);
+        telefonos=new ArrayList<String>();
+        telefonos.add("607306135");
+        telefonos.add("635815376");
+        telefonos.add("636532275");
         Guiatelefonos.put("PAU",telefonos);
-        telefonos=new ArrayList<Integer>();
-        telefonos.add(743508243);
-        telefonos.add(651097606);
-        telefonos.add(650858984);
+        telefonos=new ArrayList<String>();
+        telefonos.add("743508243");
+        telefonos.add("651097606");
+        telefonos.add("650858984");
         Guiatelefonos.put("MANEL",telefonos);
         System.out.println("Dime una persona");
         String pers= sc.nextLine().toUpperCase();
