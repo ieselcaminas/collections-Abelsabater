07e196f27ea80a9163d65d2d3f8dad2e30bd60c3
Abelsabater <abesabmun@alu.edu.gva.es>
2024-12-03 14:33:30 +0100

    Estructuras basicas

--- src/Anagramas.java
+++ src/Anagramas.java
@@ -1,18 +1,32 @@
 import java.io.BufferedReader;
+import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.io.IOException;
-import java.util.Arrays;
-import java.util.HashMap;
-import java.util.Map;
+import java.util.*;
+
 
 public class Anagramas {
+
     public static void main(String[] args) throws IOException {
-        BufferedReader reader = new BufferedReader(new FileReader("spanish-dict"));
-        String line;
-        while ((line = reader.readLine()) != null) {
-            //Trabajar con line
+        BufferedReader reader = new BufferedReader(new FileReader("file/spanish-dict (1).txt"));
+        String palabra;
+        String ordenada;
+        Map<String,ArrayList<String>>palabrass=new HashMap<>();
+        ArrayList<String>palas=new ArrayList<>();
+        while ((palabra = reader.readLine())!=null) {
+            ordenada=alphabetize(palabra);
+            palas=palabrass.get(ordenada);
+            if(palas==null){
+                palas=new ArrayList<>();
+                palas.add(palabra);
+                palabrass.put(ordenada,palas);
+            }else{
+                palas.add(palabra);
+                palabrass.put(ordenada,palas);
+            }
         }
         reader.close();
+        System.out.println(palabrass);
     }
     private static String alphabetize(String s) {
         char[] a = s.toCharArray();
