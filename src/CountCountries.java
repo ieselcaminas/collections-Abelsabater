bc122dbde65ad4b195aee076a7a134febc86de02
Abelsabater <abesabmun@alu.edu.gva.es>
2024-12-05 12:57:16 +0100

    Estructuras basicas

--- src/CountCountries.java
+++ src/CountCountries.java
@@ -1,10 +1,8 @@
 import java.io.BufferedReader;
-import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.io.IOException;
 import java.util.HashMap;
 import java.util.Map;
-
 public class CountCountries {
     public static void main(String[] args) throws IOException {
         BufferedReader reader = new BufferedReader(new FileReader("file/Colfuturo-Seleccionados.csv"));
@@ -13,8 +11,10 @@ public class CountCountries {
         while ((pais = reader.readLine())!=null) {
             String [] dades=pais.split(",");
             Integer freq = paises.get(dades[6]);
-            paises.merge(dades[6], 1, Integer::sum);
-            paises.put(dades[6], freq==null ? 1: freq+1);
+            if (freq==null)
+                paises.put(dades[6], 1);
+            else
+                paises.put(dades[6], freq+1);
         }
         System.out.println(paises);
     }
--- src/LastNameFrequency.java
+++ src/LastNameFrequency.java
@@ -1,7 +1,23 @@
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Scanner;
+
 public class LastNameFrequency {
 
-
-    public static void main(String[] args) {
-
+    public static void main(String[] args) throws IOException {
+        Scanner sc=new Scanner(System.in);
+        BufferedReader reader = new BufferedReader(new FileReader("file/LastnameFrequencies.csv"));
+        String apellido;
+        Map<String,String> apellidos=new HashMap<>();
+        while ((apellido = reader.readLine())!=null) {
+            String [] ap= apellido.split(",");
+            apellidos.put(ap[0],ap[1]);
+        }
+        System.out.println("Dime un apellido");
+        apellido= sc.nextLine().toUpperCase();
+        System.out.println(apellidos.get(apellido));
     }
 }
