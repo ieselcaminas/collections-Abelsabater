69239c806c4f75dc1892089a8d81f2ad7f7d7e25
Abelsabater <abesabmun@alu.edu.gva.es>
2024-12-12 13:29:29 +0100

    Estructuras basicas

--- src/ExpresionAritmetica.java
+++ src/ExpresionAritmetica.java
@@ -1,25 +1,56 @@
+import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.Stack;
 
 public class ExpresionAritmetica {
 
-public static boolean digito(char ex){
-    return Character.isDigit(ex);
+public static boolean digito(String ex){
+    return Character.isDigit(ex.charAt(0));
 }
-
 public static int expresionaritmetica(String hh){
-    char ex;
-    Stack<Character> pila= new Stack<>();
-    for (int i = 0; i < hh.length(); i++) {
-        ex=hh.charAt(i);
-       if(digito(ex)){
-           pila.push(ex);
+    String [] ex=hh.split(" ");
+    int n1=0;
+    int n2=0;
+    int resultat;
+    ArrayList<String> operadors=new ArrayList<>();
+    Stack<Integer> pila= new Stack<>();
+    for (int i = 0; i < ex.length; i++) {
+       if(digito(ex[i])==true){
+           pila.push(Integer.valueOf(ex[i]));
+            n1=Integer.parseInt(ex[i]);
        }else{
-
+           switch (ex[i]){
+               case "+":
+                    n1=Integer.parseInt(String.valueOf(pila.pop()));
+                    n2=Integer.parseInt(String.valueOf(pila.pop()));
+                    n1+=n2;
+                    pila.push(n1);
+                    break;
+               case "-":
+                   n1=Integer.parseInt(String.valueOf(pila.pop()));
+                   n2=Integer.parseInt(String.valueOf(pila.pop()));
+                   n1-=n2;
+                   pila.push(n1);
+                   break;
+               case "*":
+                   n1=Integer.parseInt(String.valueOf(pila.pop()));
+                   n2=Integer.parseInt(String.valueOf(pila.pop()));
+                   n1=n1*n2;
+                   pila.push(n1);
+                   break;
+               case "/":
+                   n1=Integer.parseInt(String.valueOf(pila.pop()));
+                   n2=Integer.parseInt(String.valueOf(pila.pop()));
+                   n1=n1/n2;
+                   pila.push(n1);
+                   break;
+           }
        }
     }
+    return n1;
 }
-
-public static void main(String[] args) {
-
-}
+public static void main(String[] args){
+    String llista="2 4 5 3 / * +";
+    System.out.println(expresionaritmetica(llista));
+    }
 }
