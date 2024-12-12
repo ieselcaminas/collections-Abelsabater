32c0b9b59d6a7e8a663cb16b22c2d66ac2539e6c
Abelsabater <abesabmun@alu.edu.gva.es>
2024-12-12 14:29:09 +0100

    Estructuras basicas

---
+++ src/Varios/Mediana.java
@@ -0,0 +1,22 @@
+package Varios;
+
+public class Mediana {
+public static void main(String[] args) {
+int[] num={1,2,6,17,18,22,35,46,109,143};
+    System.out.println(mediana(num));
+}
+public static double mediana(int numeros[]){
+    int s1=0;
+    int s2=0;
+    double st=0;
+    if(numeros.length%2!=0){
+        st=numeros[numeros.length/2];
+
+    }else{
+        s1=numeros.length/2;
+        s2=numeros.length/2 -1;
+        st= (double) (numeros[s1] + numeros[s2]) /2;
+    }
+    return st;
+}
+}
