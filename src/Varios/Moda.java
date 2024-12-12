4ae0ea05754e09affe6c6ed6a8243650790b2992
Abelsabater <abesabmun@alu.edu.gva.es>
2024-12-12 14:43:17 +0100

    Estructuras basicas

--- src/Varios/Moda.java
+++ src/Varios/Moda.java
@@ -1,9 +1,6 @@
 package Varios;
-
-import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.Map;
-
 public class Moda {
 public static void main(String[] args) {
     int [] num={1,1,1,1,3,4,5,5,7,7,7,8,8};
@@ -16,7 +13,7 @@ public static int moda(int[] numeros){
     for (int i = 0; i < numeros.length; i++) {
         modaa.put(numeros[i],modaa.getOrDefault(numeros[0],0)+1);
     }
-    for (Map.Entry<Integer, Integer> en: modaa.entrySet()) {
+    for (Map.Entry<Integer, Integer> en: modaa.entrySet()){
         if(en.getValue()>max){
             max=modaa.get(en.getValue());
             m= en.getKey();
