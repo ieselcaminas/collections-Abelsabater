cea618e29129575a8db18c5d78ac3a9414e203ee
Abelsabater <abesabmun@alu.edu.gva.es>
2024-12-13 11:10:20 +0100

    Estructuras basicas

--- src/Varios/Moda.java
+++ src/Varios/Moda.java
@@ -3,19 +3,25 @@ import java.util.HashMap;
 import java.util.Map;
 public class Moda {
 public static void main(String[] args){
-    int [] num={1,1,1,1,3,4,5,5,7,7,7,8,8};
+    int [] num={1,1,3,4,5,5,7,8,8,7,1,7,7};
     System.out.println(moda(num));
 }
 public static int moda(int[] numeros){
     Map<Integer,Integer> modaa=new HashMap<>();
     int m = 0;
     int max=Integer.MIN_VALUE;
+    Integer veces;
     for (int i = 0; i < numeros.length; i++) {
-        modaa.put(numeros[i],modaa.getOrDefault(numeros[0],0)+1);
+        veces=modaa.get(numeros[i]);
+        if(veces==null){
+            modaa.put(numeros[i],1);
+        }else{
+            modaa.put(numeros[i],veces+1);
+        }
     }
     for (Map.Entry<Integer, Integer> en: modaa.entrySet()){
         if(en.getValue()>max){
-            max=modaa.get(en.getValue());
+            max=en.getValue();
             m= en.getKey();
         }
     }
