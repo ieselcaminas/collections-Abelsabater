cd6c926a0ee9f637fa4478f47230373881afd992
Abelsabater <abesabmun@alu.edu.gva.es>
2025-01-10 13:37:44 +0100

    Estructuras basicas

--- src/Comparable/Pais.java
+++ src/Comparable/Pais.java
@@ -1,26 +1,20 @@
 package Comparable;
-
 public class Pais  implements Comparable<Pais>{
 private String nombre;
 private int n_analfabetos;
-
 public Pais(String nombre, int n_analfabetos) {
     this.nombre = nombre;
     this.n_analfabetos = n_analfabetos;
 }
-
 public String getNombre() {
     return nombre;
 }
-
 public void setNombre(String nombre) {
     this.nombre = nombre;
 }
-
 public int getN_analfabetos() {
     return n_analfabetos;
 }
-
 public void setN_analfabetos(int n_analfabetos) {
     this.n_analfabetos = n_analfabetos;
 }
