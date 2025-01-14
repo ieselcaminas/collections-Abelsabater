596531e9965f5cfb3184955979616184576e95bd
Abelsabater <abesabmun@alu.edu.gva.es>
2025-01-14 14:23:10 +0100

    Estructuras basicas

--- src/Comparable/Niños.java
+++ src/Comparable/Niños.java
@@ -1,26 +1,20 @@
 package Comparable;
-
 public class Niños implements Comparable<Niños>{
 private int puntuacion;
 private int pesoregalos;
-
 public Niños(int puntuacion, int pesoregalos) {
     this.puntuacion = puntuacion;
     this.pesoregalos = pesoregalos;
 }
-
 public int getPuntuacion() {
     return puntuacion;
 }
-
 public void setPuntuacion(int puntuacion) {
     this.puntuacion = puntuacion;
 }
-
 public int getPesoregalos() {
     return pesoregalos;
 }
-
 public void setPesoregalos(int pesoregalos) {
     this.pesoregalos = pesoregalos;
 }
