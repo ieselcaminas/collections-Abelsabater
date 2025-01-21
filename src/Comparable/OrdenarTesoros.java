5f4d7a7d621f0dd77abdf01f540e96011cd11e68
Abelsabater <abesabmun@alu.edu.gva.es>
2025-01-21 13:42:50 +0100

    Estructuras basicas

--- src/Comparable/OrdenarTesoros.java
+++ src/Comparable/OrdenarTesoros.java
@@ -4,11 +4,15 @@ import java.util.List;
 public class OrdenarTesoros {
 public static void main(String[] args) {
     List <Tesoros> tesorosList=new ArrayList<>();
-    Tesoros tesoros=new Tesoros(10,2,"flecha");
+    Tesoros tesoros=new Tesoros(5,1,"flecha");
     tesorosList.add(tesoros);
-    tesoros=new Tesoros(2,3,"sarcofago");
+    tesoros=new Tesoros(1000,1000,"sarcofago");
     tesorosList.add(tesoros);
-    tesoros=new Tesoros(10,8,"sandalia");
+    tesoros=new Tesoros(10,2,"sandalia");
+    tesorosList.add(tesoros);
+    tesoros=new Tesoros(1000,10,"mascara");
+    tesorosList.add(tesoros);
+    tesoros=new Tesoros(10,2,"arco");
     tesorosList.add(tesoros);
     tesorosList.stream()
             .sorted()
